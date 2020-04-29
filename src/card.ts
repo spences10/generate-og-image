ede330148f648acc51568622ce0520df25d65a77
spences10 <spences10apps@gmail.com>
2020-04-29 11:54:36 +0100

    Local html working with image

---
+++ src/card.ts
@@ -0,0 +1,21 @@
+import { IncomingMessage, ServerResponse } from 'http'
+import { parseReqs } from './parser'
+import { getHtml } from './template'
+export default async function handler(
+  req: IncomingMessage,
+  res: ServerResponse
+) {
+  try {
+    const parsedReqs = parseReqs(req)
+    const html = getHtml(parsedReqs)
+
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end(html)
+  } catch (e) {
+    res.statusCode = 500
+    res.setHeader('Content-Type', 'text/html')
+    res.end('<h1>Internal Error</h1><p>Sorry, an error. I derp!</p>')
+    console.log(e)
+  }
+}
