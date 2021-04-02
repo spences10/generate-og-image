5045e53e3763c68bb921b7fbed488de23b4607d2
spences10 <spences10apps@gmail.com>
2021-04-02 19:37:08 +0100

    add fobio site

--- src/template-html.ts
+++ src/template-html.ts
@@ -159,6 +159,57 @@ export function getCss(website: any) {
         color: #4a5568;
       }
       `,
+    'fabiorosado.dev': `
+      body {
+        --color-1: rgb(252, 50, 75);
+        --color-2: rgb(39, 27, 64);
+        background: linear-gradient(0.25turn, var(--color-1), var(--color-2));
+        height: 100vh;
+        font-family: "Space Mono", sans-serif;
+        font-size: 18px;
+        padding: 50px;
+      }
+      .container {
+        position: relative;
+        height: calc(100vh - 100px);
+        padding: 50px;
+        background: #f7fafc;
+        box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+        border-radius: 14px;
+      }
+      .title {
+        font-size: 5.4em;
+        height: 4em;
+        overflow: hidden;
+        color: #1a202c;
+        word-spacing: -20px;
+        background: linear-gradient(0.25turn, var(--color-2), var(--color-1));
+        -webkit-background-clip: text;
+        background-clip: text;
+        -webkit-text-fill-color: transparent;
+      }
+      .author {
+        position: absolute;
+        bottom: 0px;
+        left: 0px;
+        padding: 50px;
+        font-size: 3em;
+        color: #4a5568;
+      }
+      .author-image {
+        width: 1.5em;
+        border-radius: 50%;
+        margin-bottom: -9px;
+      }
+      .website {
+        position: absolute;
+        bottom: 0px;
+        right: 0px;
+        padding: 50px;
+        font-size: 2em;
+        color: #4a5568;
+      }
+      `,
   }
 
   return sitesCss[website]
